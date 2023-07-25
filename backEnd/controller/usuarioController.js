270bc15b0f43263cd3d9a5c21cdf6f900e7cf846
Escritorio-Home <lucaspodkowa@gmail.com>
2023-07-24 21:13:20 -0300

    ojala no sea una macana

---
+++ backEnd/controller/usuarioController.js
@@ -0,0 +1,63 @@
+require('rootpath')();
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var usuarioDb = require("model/user.js");
+
+//req : es lo que llega desde el frontend (en nuestro caso Postman)
+//res : respuesta enviada desde el servidor al frontend
+
+//atendiendo el endpoint /api/persona mediante el metodo GET 
+// |--> llamar a la funcion getAll() que está en el archivo encargado de hestionar lo relacionado a la tabla PERSONA en la BD
+//      y procesara la respuesta en una funcion callback
+// |--> GetAll() enviara como respuesta un error (que le enviará la base de datos) o los datos en caso de exito   
+
+
+app.get('/', (req, res) => {
+
+    usuarioDb.getAll((err, resultado) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(resultado);
+        }
+    });
+
+});
+
+
+//---------------------------------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------------------------------
+
+
+app.post('/', (req, res) => {
+
+    let usuario = req.body;
+    usuarioDb.create(usuario, (err, resultado) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(resultado);
+        }
+    });
+
+});
+
+module.exports = app;
+
+
+lucas, pod, mail
+lucre, mencia, mail
+brenda, staudt, mail
+marco, mail
+
+var uno = {
+
+    "nombre": "",
+    "apellido": null,
+    "mail": ""
+};
+
+

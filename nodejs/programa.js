f134b03bca9fd72290d49940a0119d7306d06f85
lucaspodkowa <lucaspodkowa@gmail.com>
2023-07-25 18:08:50 -0300

    Delete programa.js

--- nodejs/programa.js
+++
@@ -1,9 +0,0 @@
-// Declaro la función sumar y paso como parametro 2 numeros
-function sumar(num1, num2) {
-    resultado = num1 + num2;
-    console.log("La suma es: "+resultado)
-    } 
-    // Declaró la explotación de la función
-    module.exports = {
-    "sumar": sumar
-    }
